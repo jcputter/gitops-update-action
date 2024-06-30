12016bcab1b128575c53bce55192cd0cb34ad467
JC Putter <jcputter@gmail.com>
2024-06-30 12:24:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ const configureSSH = async (deployKey) => {
     await fs.mkdir(sshDir, { recursive: true });
     const knownHosts = path.join(sshDir, 'known_hosts');
     const deployKeyPath = path.join(sshDir, 'id_rsa');
+    const deployKey = deployKey.replace(/\\n/g, '\n');
 
     await fs.writeFile(deployKeyPath, deployKey, { mode: 0o600 });
     await execPromise(`ssh-keyscan github.com >> ${knownHosts}`);
